bb9b6cfb6f2b449e1d52bd27f7cb5d5b8131847d
shichuanliang <1247728163@qq.com>
2019-07-09 18:57:34 +0800

    给实体类添加属性

---
+++ src/main/java/aaa/test_git_01/entity/Dept.java
@@ -0,0 +1,22 @@
+package aaa.test_git_01.entity;
+
+public class Dept {
+    private Integer depton;
+    private String dname;
+
+    public Integer getDepton() {
+        return depton;
+    }
+
+    public void setDepton(Integer depton) {
+        this.depton = depton;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+}
