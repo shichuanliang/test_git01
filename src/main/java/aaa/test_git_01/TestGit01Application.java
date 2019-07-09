db97d8c9d950c4af0a9bf179e94b593b6de8d346
shichuanliang <1247728163@qq.com>
2019-07-09 16:18:14 +0800

    项目架构的搭建

---
+++ src/main/java/aaa/test_git_01/TestGit01Application.java
@@ -0,0 +1,13 @@
+package aaa.test_git_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGit01Application.class, args);
+    }
+
+}
---
+++ src/test/java/aaa/test_git_01/TestGit01ApplicationTests.java
@@ -0,0 +1,16 @@
+package aaa.test_git_01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestGit01ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
